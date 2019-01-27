d9277bd44b675672ab290867a2b7fba5761f0a71
木下晃兵 <kinoshitakohei@MacBook-Air-2.local>
2019-01-27 18:11:50 +0900

    [resubmit]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
 	before_action :authenticate_user!, only: [:index, :destroy, :show, :update, :create, :new]
-	before_action :find_book, only: [:show, :edit, :update, :destroy]
+	# before_action :find_book, only: [:show, :edit, :update, :destroy]
 	before_action :correct_user, only: [:edit, :update]
 	# before_action :validate_user, only: [:show, :edit, :update, :destroy]
 
 	def show
-		# @books = Book.find(params[:id])
+		@books = Book.find(params[:id])
 		@book = Book.new
 		@user = @books.user
 	end
@@ -35,10 +35,14 @@ class BooksController < ApplicationController
 		@book = Book.new
 	end
 
+	  def edit
+	  	@books = Book.find(params[:id])
+	  end
+
 	def update
 		if @book.update(book_params)
 			flash[:notice] = "Book was successfully updated."
-			redirect_to @book
+			redirect_to book_path(@book.id)
 		else
 			# render :edit, alert: "error"
 	        @alert = "error"
@@ -47,20 +51,16 @@ class BooksController < ApplicationController
 	end
 
 	def destroy
-		if @book.destroy
-			flash[:notice] = "successfully destroy"
-			redirect_to root_path
-		else
-			# redirect_to root_path, alert: "error"
-			@alert = "error"
-	        render :root
-		end
+		@book = Book.find(params[:id])
+		@book.destroy
+		flash[:notice] = "successfully destroy"
+		redirect_to books_path
 	end
 
 	private
-	def find_book
-		@books = Book.find(params[:id])
-	end
+	# def find_book
+	# 	@book = Book.find(params[:i1d])
+	# end
 
 	def book_params
 		params.require(:book).permit(:title, :body)
